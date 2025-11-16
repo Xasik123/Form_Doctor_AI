cca489cf10b28570528e9eb86a3f89471ea8e309
Xasik123 <aslansajhin9@gmail.com>
2025-11-17 00:05:15 +0500

    Create post_setup.sh

---
+++ .streamlit/post_setup.sh
@@ -0,0 +1,4 @@
+# Скрипт для принудительной установки MediaPipe и OpenCV
+# Используем проверенные whl-файлы для Python 3.10
+pip install mediapipe==0.10.21 --no-deps
+pip install opencv-python==4.11.0.86 --no-deps
