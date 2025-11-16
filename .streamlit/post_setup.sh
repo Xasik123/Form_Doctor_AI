6535244ac5e952785cf6505ce96ba44aa78ac0e8
Xasik123 <aslansajhin9@gmail.com>
2025-11-17 00:07:06 +0500

    Update post_setup.sh

--- .streamlit/post_setup.sh
+++ .streamlit/post_setup.sh
@@ -1,4 +1,11 @@
-# Скрипт для принудительной установки MediaPipe и OpenCV
-# Используем проверенные whl-файлы для Python 3.10
-pip install mediapipe==0.10.21 --no-deps
-pip install opencv-python==4.11.0.86 --no-deps
+#!/bin/bash
+# Обновление системных пакетов
+sudo apt-get update -y
+
+# Установка OpenCV и MediaPipe без учета зависимостей
+# Используем прямой путь к pip в среде Streamlit Cloud
+/home/adminuser/venv/bin/pip install mediapipe==0.10.21 --no-deps
+/home/adminuser/venv/bin/pip install opencv-python==4.11.0.86 --no-deps
+
+# Установка остальных пакетов
+/home/adminuser/venv/bin/pip install -r requirements.txt
