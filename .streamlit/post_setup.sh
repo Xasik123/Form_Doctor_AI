fa696f77980e49b86bdff2d901c9249d2f7ce5b9
Xasik123 <aslansajhin9@gmail.com>
2025-11-17 00:09:10 +0500

    Update post_setup.sh

--- .streamlit/post_setup.sh
+++ .streamlit/post_setup.sh
@@ -2,10 +2,8 @@
 # Обновление системных пакетов
 sudo apt-get update -y
 
-# Установка OpenCV и MediaPipe без учета зависимостей
-# Используем прямой путь к pip в среде Streamlit Cloud
-/home/adminuser/venv/bin/pip install mediapipe==0.10.21 --no-deps
-/home/adminuser/venv/bin/pip install opencv-python==4.11.0.86 --no-deps
+# Установка MediaPipe (самостоятельно установит OpenCV)
+/home/adminuser/venv/bin/pip install mediapipe==0.10.21 --no-cache-dir
 
 # Установка остальных пакетов
 /home/adminuser/venv/bin/pip install -r requirements.txt
